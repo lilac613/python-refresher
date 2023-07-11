0c6db332069ec57b367ec2b2036d1d1ec4d75e15
lilac613 <yablondani@gmail.com>
2023-07-11 16:11:01 -0400

    added test cases for functions in hello.py and added numpy to requirements.txt

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-# import numpy as np
+import numpy as np
 
 
 def hello():
-    print("Hello, world!")
+    return "Hello, world!"
 
 
 def add(a, b):
@@ -23,37 +23,29 @@ def div(a, b):
     return a / b
 
 
-# def sqrt(a):
-#     return np.sqrt(a)
+def sqrt(a):
+    return np.sqrt(a)
 
+def power(a, b):
+    return np.power(a, b)
 
-# def power(a, b):
-#     return np.power(a, b)
+def log(a):
+    return np.log(a)
 
+def exp(a):
+    return np.exp(a)
 
-# def log(a):
-#     return np.log(a)
+def sin(a):
+    return np.sin(a)
 
+def cos(a):
+    return np.cos(a)
 
-# def exp(a):
-#     return np.exp(a)
+def tan(a):
+    return np.tan(a)
 
-
-# def sin(a):
-#     return np.sin(a)
-
-
-# def cos(a):
-#     return np.cos(a)
-
-
-# def tan(a):
-#     return np.tan(a)
-
-
-# def cot(a):
-#     return 1 / np.tan(a)
-
+def cot(a):
+    return 1 / np.tan(a)
 
 def __main__():
     hello()
@@ -61,3 +53,53 @@ def __main__():
 
 if __name__ == "__main__":
     __main__()
+
+# test functions
+print(add (3,5))
+print(add (-3,-5))
+print(add(-3,5))
+
+print(sub (3,5))
+print(sub (-3,-5))
+print(sub (-3,5))
+
+print(mul (3,5))
+print(mul (-3,-5))
+print(mul (-3,5))
+
+print(div (3,5))
+print(div (0,5))
+print(div (5,0))
+
+print(sqrt(-4))
+print(sqrt(4))
+print(sqrt(25))
+
+print(power(2,3))
+print(power(-2,3))
+print(power(2,-3))
+
+print(log(0))
+print(log(1))
+print(log(100))
+
+print(exp(3))
+print(exp(0))
+print(exp(-3))
+
+print(sin(0))
+print(sin(3.14/2))
+print(sin(3.14*1.5))
+
+print(cos(0))
+print(cos(3.14/2))
+print(cos(3.14*1.5))
+
+print(tan(0))
+print(tan(3.14/2))
+print(tan(3.14))
+
+print(cot(0))
+print(cot(3.14/2))
+print(cot(3.14))
+
