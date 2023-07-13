091e94b1821bb6c5cc4d7a1ca4df4606c4a86496
lilac613 <yablondani@gmail.com>
2023-07-13 11:20:44 -0400

    Made code more readable

--- physics.py
+++ physics.py
@@ -1,22 +1,24 @@
-def calculate_buoyancy(v, density_fluid):
+g = 9.81
+water_density = 1000
+def calculate_buoyancy(V, density_fluid):
     '''Calculates the buoyancy forced exerted an object submerged in water'''
-    if v<0:
+    if V <= 0:
         raise ValueError("Volume cannot be negative!")
-    if density_fluid < 0:
+    if density_fluid <= 0:
         raise ValueError("The density of the fluid cannot be negative!")
-    return v*density_fluid*9.81
+    return V*density_fluid*g
 
-def will_it_float(v, mass):
+def will_it_float(V, mass):
     '''Determines whether an object will float or sink in water'''
-    if v < 0:
+    if V <= 0:
         raise ValueError("Volume cannot be negative!")
-    if mass < 0:
+    if mass <= 0:
         raise ValueError("Mass cannot be negative!")
-    if round(mass*9.81,3) == round(v*9.81*1000,3):
+    if round(mass*g,3) == round(V*g*water_density,3):
         return None
-    return mass*9.81 < v*9.81*1000
+    return mass*g < V*g*water_density
 
 def calculate_pressure(depth):
     '''Calculates pressure at a given depth in water'''
-    return 1000*9.81*abs(depth)
+    return water_density*g*abs(depth)
 
